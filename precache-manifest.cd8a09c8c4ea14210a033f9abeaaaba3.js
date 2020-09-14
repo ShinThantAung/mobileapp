0964dbc6169404ea9bafcdf2e491516d686bfd7e
Big 9 <big9@MacBook-Pro.local>
2020-09-14 21:29:39 +0800

    Updates

---
+++ precache-manifest.cd8a09c8c4ea14210a033f9abeaaaba3.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fde4d0dfa2d4ca7b8c2cc17ec153583",
+    "url": "/chat/index.html"
+  },
+  {
+    "revision": "90b1f98634dbe96e3f37",
+    "url": "/chat/static/css/main.6eeae962.chunk.css"
+  },
+  {
+    "revision": "edcaee01291295ec4884",
+    "url": "/chat/static/js/2.e18a7f44.chunk.js"
+  },
+  {
+    "revision": "97c0b5374351518bd0674c0fa3f02654",
+    "url": "/chat/static/js/2.e18a7f44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90b1f98634dbe96e3f37",
+    "url": "/chat/static/js/main.5b848571.chunk.js"
+  },
+  {
+    "revision": "ad8dbb0c9fbade2f514d",
+    "url": "/chat/static/js/runtime-main.623d0f6d.js"
+  },
+  {
+    "revision": "9299706f23346da2776f4aef1e52996a",
+    "url": "/chat/static/media/ic_wave_hand.9299706f.png"
+  },
+  {
+    "revision": "477fc8ddd40d200fe8f2635b5151a873",
+    "url": "/chat/static/media/mimi1.477fc8dd.gif"
+  },
+  {
+    "revision": "82a783353a27abe59feafd40bc686af1",
+    "url": "/chat/static/media/mimi2.82a78335.gif"
+  },
+  {
+    "revision": "477fc8ddd40d200fe8f2635b5151a873",
+    "url": "/chat/static/media/mimi3.477fc8dd.gif"
+  },
+  {
+    "revision": "6b65742393c65511ba223ec388c85ecc",
+    "url": "/chat/static/media/mimi4.6b657423.gif"
+  },
+  {
+    "revision": "609127cb5129f8e218fb57f059497c87",
+    "url": "/chat/static/media/mimi5.609127cb.gif"
+  },
+  {
+    "revision": "1ce35a6b3c21cc308a7683dfbea81c63",
+    "url": "/chat/static/media/mimi6.1ce35a6b.gif"
+  },
+  {
+    "revision": "ace546bd302229af3a0c7e7221d2dc10",
+    "url": "/chat/static/media/mimi7.ace546bd.gif"
+  },
+  {
+    "revision": "6646a3dec849498313a17c7de5d2ea5c",
+    "url": "/chat/static/media/mimi8.6646a3de.gif"
+  },
+  {
+    "revision": "b5ec7c32285b24c09eca4646a4511518",
+    "url": "/chat/static/media/mimi9.b5ec7c32.gif"
+  }
+]);
